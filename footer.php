f9567607397b758f47e78cfc16b9ade9bfe348dd
Russ Brown <poa02rb@yahoo.co.uk>
2013-12-01 05:16:10 +0000

    Cookie Directive removed
    and GA put in footer

--- footer.php
+++ footer.php
@@ -70,17 +70,20 @@
 
 	</div><!-- #page .hfeed .site -->
 
-<!-- the cookies directive notifier -->
+<!-- enter google analytics here -->
+
 	<script type="text/javascript">
-	    function cookiesDirectiveScriptWrapper(){
-	</script>
-	<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/cookies.js">
-	</script>
-	<script type="text/javascript">
-	    // The position of the disclosure ('top' or 'bottom')
-	    // Number of times to display disclosure. Enter 0 to show it forever!!!!
-	    // The URI of your privacy policy
-	    cookiesDirective('bottom',1,'http://www.aboutcookies.org');
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-32142501-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
 	</script>
 
 </body>
--- header.php
+++ header.php
@@ -34,22 +34,6 @@
 <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/ie.css">
 <![endif]-->
 
-<!-- enter google analytics here -->
-
-<script type="text/javascript">
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-32142501-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-</script>
-
 <?php wp_head(); ?>
 
 	<!-- load jquery library -->
